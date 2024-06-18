847639f09eb0e51a71d1372a5d81f816644f8e98
asom1989 <91065346+asom1989@users.noreply.github.com>
2024-06-18 20:10:59 +0200

    add services component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Services />
     </>
   );
 }
