c0b67bb39d522779d6b955fbcb36530f8227333f
asom1989 <91065346+asom1989@users.noreply.github.com>
2024-06-23 19:15:22 +0200

    add authors page

--- src/pages/Authors/Authors.jsx
+++ src/pages/Authors/Authors.jsx
@@ -1,5 +1,38 @@
-function Authors() {
-  return <h1>Authors</h1>;
-}
+import "./Authors.css";
+import { useState } from "react";
+import { authors } from "../../assets/data/authors";
+
+const Authors = () => {
+  const [search, setSearch] = useState("");
+
+  //console.log(authors.filter(a => a.name.toLowerCase().includes(search)));
+
+  return (
+    <section className="authors">
+      <div className="authors-search-wrapper">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="search"
+          placeholder="Search in authors"
+        />
+      </div>
+      <div className="authors-wrapper">
+        {authors
+          .filter((a) => a.name.toLowerCase().includes(search))
+          .map((author) => (
+            <div key={author.id} className="author">
+              <img
+                src={author.image}
+                alt={author.name}
+                className="author-img"
+              />
+              <h2 className="author-name">{author.name}</h2>
+            </div>
+          ))}
+      </div>
+    </section>
+  );
+};
 
 export default Authors;
