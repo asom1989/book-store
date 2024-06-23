2c270d59488c5c95c3730dd62aab03f90765072b
asom1989 <91065346+asom1989@users.noreply.github.com>
2024-06-23 16:11:24 +0200

    add cart component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import "./Cart.css";
+import { cartInfo } from "../../assets/data/cart";
+import OrderSummary from "./OrderSummary";
+import CartItem from "./CartItem";
 
 function Cart() {
-  return <div>Cart</div>;
+  const totalPrice = cartInfo.reduce(
+    (acc, cur) => acc + cur.price * cur.quantity,
+    0
+  );
+  return (
+    <div className="cart">
+      <h1 className="cart-title">Your Shopping Cart</h1>
+      <div className="cart-wrapper">
+        <div className="cart-items">
+          {cartInfo.map((item) => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </div>
+        <OrderSummary totalPrice={totalPrice} />
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
